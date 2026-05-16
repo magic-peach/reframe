dfae3d170c0cf9ae8d2301c2098e11dbc0694e56
HarshaNaidu11 <harshavardhannaidu1111@gmail.com>
2026-05-16 14:37:28 +0530

    Fixed lint errors1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,10 @@ export default function Home() {
       >
         ⭐ Star on GitHub
       </a>
-      <VideoEditor />
+      <main id="main-content" tabIndex={-1}>
+        <VideoEditor />
+      </main>
+      
     </>
   );
 }
